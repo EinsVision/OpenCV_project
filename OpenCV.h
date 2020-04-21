5aeea361c854db93fb0b2e5af67bd62ee6aff50f
EinsVision <63795065+EinsVision@users.noreply.github.com>
2020-04-21 14:32:59 +0900

    change header file

--- OpenCV.h
+++ OpenCV.h
@@ -1,3 +1,17 @@
 #pragma once
 #include <iostream>
 #include "opencv2/opencv.hpp"
+
+// Image 클래스 선언
+
+class Image {
+
+private:
+    int channel;
+    double height;
+    double width;
+
+public:
+    void set_Image(int channel, double height, double width);
+    void show_Image();
+}; 
